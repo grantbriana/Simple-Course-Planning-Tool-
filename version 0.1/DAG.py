76f5c79410792249598d9cb9d4de8d81263337f4
Briana Grant <66385889+grantbriana@users.noreply.github.com>
2022-10-02 19:30:58 -0500

    version 0.1

---
+++ version 0.1/DAG.py
@@ -0,0 +1,80 @@
+import fileReader as fr
+import networkx as nx
+from matplotlib import pyplot as plt
+#import numpy as np
+from networkx.drawing.nx_pydot import graphviz_layout
+import scheduleGenerator as generator
+#from xlwt import Workbook
+import xlwt
+import scheduleGenerator as generator
+wb = xlwt.Workbook()
+
+
+#------------------------------------------------------------DAG tree
+# group nodes by column
+left_nodes = []
+middle_nodes = []
+right_nodes = []
+
+graph = nx.DiGraph()
+graph.add_node("Senior")
+graph.add_node("Senior/Junior")
+
+for course in fr.courses:
+    graph.add_node(str(course.name))
+j = 0
+for classes in fr.courses:
+    for classes2 in fr.courses:
+        if classes2.prereq == classes.name:
+            graph.add_edge(classes.name, classes2.name)
+        elif classes.prereq == "Senior":
+            graph.add_edge(classes.name, "Senior")
+        elif classes.prereq == "Senior/Junior":
+            graph.add_edge(classes.name, "Senior")
+            graph.add_edge(classes.name, "Senior/Junior")
+for classes in fr.courses:
+    if graph.nodes(classes):
+        graph.add_node(classes.name)
+
+
+for classes in fr.courses:
+    if classes.prereq == "":
+        left_nodes.append(classes.name)
+    else:
+        for classes2 in fr.courses:
+            if classes.prereq != "" and classes.name == classes2.prereq:
+                middle_nodes.append(classes.name)
+            elif classes.prereq != "" and classes.name != classes2.prereq:
+                right_nodes.append(classes)
+
+# pos = nx.DiGraph({n: (0, i) for i, n in enumerate(left_nodes)})
+# pos.update({n: (1, i + 0.5) for i, n in enumerate(middle_nodes)})
+# pos.update({n: (2, i + 0.5) for i, n in enumerate(right_nodes)})
+
+plt.figure(1,figsize=(12,12))
+print(len(graph.nodes()))
+d = dict(graph.degree)
+list(nx.topological_sort(graph))
+plt.tight_layout()
+nx.draw(graph, with_labels=True,font_weight='bold', node_size=500)
+ax = plt.gca()
+ax.margins(0.01)
+plt.axis("off")
+plt.savefig("DAG.png")
+plt.show()
+i = 0;
+list(reversed(list(nx.topological_sort(graph))))
+graph.edges()
+
+j =0
+sheet1 = wb.add_sheet('Sheet 1')
+for i in generator.courseArray:
+    for k in i:
+        sheet1.write(j, 0, k.name)
+        sheet1.write(j, 1, k.description)
+        sheet1.write(j, 2, str(k.hours))
+        sheet1.write(j, 3, k.prereq)
+        j = j + 1
+    j = j + 1
+
+wb.save('SCP_Tool_output.xls')
---
+++ version 0.1/scheduleGenerator.py
@@ -0,0 +1,133 @@
+import fileReader as fr
+
+tempArray = []
+courseArray = []
+classesArrayStage = []
+Working = True
+
+for i in fr.courses:
+    classesArrayStage.append(i)
+
+#checks if courses prereq has been taken
+def prereqCheck(course):
+    if course.prereq != "":
+        istaken = False
+        for course2 in fr.courses:
+            if (course.prereq == course2.name) and course2.taken == True:
+                istaken = True
+        return istaken
+    else:
+        return False
+   
+        
+#builds each semester based on if it is during the
+def scheduleCreate():
+    while Working:
+        tempArray = []
+        j = 0
+        for course in fr.courses:
+            if(j < 12):
+                if course.spring == True:
+                    if (course.taken == False):
+                        if tempArray:
+                            isAPrereq = False
+                            for i in tempArray:
+                                if i.name == course.prereq:
+                                    isAPrereq = True
+                                else:
+                                    isAPrereq = False
+                            if ((isAPrereq == False and prereqCheck(course) == True) or course.prereq ==""):
+                                tempArray.append(course)
+                                j = j + course.hours
+                        elif not tempArray:
+                            tempArray.append(course)
+                            j = j + course.hours
+
+                #else:
+
+        if(not tempArray):
+            break
+        courseArray.append(tempArray)
+        for i in tempArray:
+            i.taken = True
+
+        tempArray = []
+        j = 0
+        for course in fr.courses:
+            if(j < 12):
+                if course.summer == True:
+                    if (course.taken == False):
+                        if tempArray:
+                            isAPrereq = False
+                            for i in tempArray:
+                                if i.name == course.prereq:
+                                    isAPrereq = True
+                                else:
+                                    isAPrereq = False
+                            if ((isAPrereq == False and prereqCheck(course) == True) or course.prereq ==""):
+                                tempArray.append(course)
+                                j = j + course.hours
+                        elif not tempArray:
+                            tempArray.append(course)
+                            j = j + course.hours
+
+                #else:
+
+        if(not tempArray):
+            break
+        courseArray.append(tempArray)
+        for i in tempArray:
+            i.taken = True
+
+        tempArray = []
+        j = 0
+        for course in fr.courses:
+            if(j < 12):
+                if course.fall == True:
+                    if (course.taken == False):
+                        if tempArray:
+                            isAPrereq = False
+                            for i in tempArray:
+                                if i.name == course.prereq:
+                                    isAPrereq = True
+                                else:
+                                    isAPrereq = False
+                            if ((isAPrereq == False and prereqCheck(course) == True) or course.prereq ==""):
+                                tempArray.append(course)
+                                j = j + course.hours
+                        elif not tempArray:
+                            tempArray.append(course)
+                            j = j + course.hours
+
+                #else:
+
+        if(not tempArray):
+            break
+        courseArray.append(tempArray)
+        for i in tempArray:
+            i.taken = True
+
+
+#out the classes. For testing purposes
+k = 1
+def season():
+    if k in (1, 4, 7):
+        return "Spring"
+    elif k in (2, 5, 8):
+        return  "Summer"
+    elif k in (3, 6, 9):
+        return  "Fall"
+
+
+'''
+fr.getUserTrack(fr.drop)
+fr.getNeededClasses()
+#calls the function
+scheduleCreate()
+'''
+
+for course in courseArray:
+    print("\n" + "Semester: " + str(k) + " " + season() + "\n")
+    k = k + 1
+    for i in course:
+        print(i)
